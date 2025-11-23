56b1e4c27abdc08faecaa7ec2a5ad90297b05576
akashbinu699 <akashbinu699@gmail.com>
2025-11-23 20:50:19 +0530

    Add output for endpoint ID in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,10 @@ output "endpoint_name" {
   value = google_vertex_ai_endpoint.endpoint.name
 }
 
+output "endpoint_id" {
+  value = google_vertex_ai_endpoint.main.id
+}
+
 output "model_name" {
   value = google_vertex_ai_model.sdxl_model.name
 }
