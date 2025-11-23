68330830d7341179cfeb73585e105de51a00dae1
akashbinu699 <akashbinu699@gmail.com>
2025-11-23 23:01:32 +0530

    Refactor outputs to read from files
    Updated outputs to read from files and removed unused outputs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,7 @@
-
 output "endpoint_id" {
-  description = "The deployed Vertex AI endpoint resource name"
-  value       = google_vertex_ai_endpoint.endpoint.name
+  value = chomp(file("endpoint_id.txt"))
 }
 
-output "endpoint_display_name" {
-  description = "Friendly endpoint name"
-  value       = google_vertex_ai_endpoint.endpoint.display_name
-}
-
-output "region" {
-  description = "Region where Vertex resources are deployed"
-  value       = var.region
+output "model_id" {
+  value = chomp(file("model_id.txt"))
 }
