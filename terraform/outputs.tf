cc186e5a9224e76d314eb0abf9d007b9b08f4aec
akashbinu699 <akashbinu699@gmail.com>
2025-11-23 21:43:54 +0530

    Refactor outputs in outputs.tf
    Removed duplicate model_name output and added deployment_id output.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,11 @@
-# terraform/outputs.tf
+output "model_name" {
+  value = google_vertex_ai_model.sdxl_model.name
+}
+
 output "endpoint_name" {
   value = google_vertex_ai_endpoint.endpoint.name
 }
 
-output "endpoint_id" {
-  value = google_vertex_ai_endpoint.main.id
-}
-
-output "model_name" {
-  value = google_vertex_ai_model.sdxl_model.name
+output "deployment_id" {
+  value = google_vertex_ai_endpoint_deployed_model.deployment.id
 }
