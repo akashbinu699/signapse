3515586de840c51c482829147f5fa59662371a5a
akashbinu699 <akashbinu699@gmail.com>
2025-11-23 23:27:46 +0530

    Refactor outputs to use file existence checks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,9 @@
-data "local_file" "endpoint" {
-  depends_on = [null_resource.create_endpoint]
-  filename   = "${path.module}/endpoint_id.txt"
-}
-
-data "local_file" "model" {
-  depends_on = [null_resource.register_model]
-  filename   = "${path.module}/model_id.txt"
+output "model_id" {
+  value = fileexists("model_id.txt") ? chomp(file("model_id.txt")) : null
+  description = "Uploaded model resource ID"
 }
 
 output "endpoint_id" {
-  value = trimspace(data.local_file.endpoint.content)
-}
-
-output "model_id" {
-  value = trimspace(data.local_file.model.content)
+  value = fileexists("endpoint_id.txt") ? chomp(file("endpoint_id.txt")) : null
+  description = "Deployed endpoint ID"
 }
